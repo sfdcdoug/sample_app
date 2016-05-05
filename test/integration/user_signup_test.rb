407da5d7f6c0f5394a1e92077ba4ac0add000b86
Doug Bitting <dbitting@salesforce.com>
2016-05-04 17:30:24 -0700

    Finish user signup

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -11,4 +11,15 @@ class UserSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: "Test User",
+                                            email: "foo@example.com",
+                                            password: "foobar",
+                                            password_confirmation: "foobar" }
+    end
+    assert_template 'users/show'
+  end
 end
