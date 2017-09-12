9adabdd122c3cb5714123b3a473c5523fba80f87
Doug Bitting <dbitting@salesforce.com>
2017-09-11 20:12:37 -0700

    logging headers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+logger.warn "*** BEGIN RAW REQUEST HEADERS ***"
+self.request.env.each do |header|
+  logger.warn "HEADER KEY: #{header[0]}"
+  logger.warn "HEADER VAL: #{header[1]}"
+end
+logger.warn "*** END RAW REQUEST HEADERS ***"
   end
 
   def help
